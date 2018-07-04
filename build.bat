1064964120e870a53d94a08b53b99fbb996ccc17
draculantern <draculantern@gmail.com>
2018-07-04 14:48:14 -0400

    removed unused code

--- build.bat
+++ build.bat
@@ -6,11 +6,11 @@ pushd bin
 set BinaryName=highp
 set ExeName=%BinaryName%.exe
 
-set CompilerFlags= -nologo -I..\src\ ^
- -GR- -Gm- -EHa- -Oi -fp:fast -fp:except- -GS- -Gs0x100000 ^
--D_CRT_SECURE_NO_WARNINGS=1 -DNOMINMAX=1 ^
--W4 -WX -we4061 -wd4100 -wd4189 -wd4127 -wd4505 -wd4201 ^
--wd4533 -wd4389 -wd4244 -wd4820 
+set CompilerFlags= /nologo /I..\src\ ^
+/GR- /Gm- /EHa- /Oi /fp:fast /fp:except- /GS- /Gs0x100000 ^
+/D_CRT_SECURE_NO_WARNINGS=1 /DNOMINMAX=1 ^
+/W4 /WX /we4061 /wd4100 /wd4189 /wd4127 /wd4505 /wd4201 ^
+/wd4533 /wd4389 /wd4244 /wd4820 
 
 set LinkFlags= /INCREMENTAL:NO /MACHINE:X64 /NODEFAULTLIB /STACK:0x100000,0x100000 /SUBSYSTEM:CONSOLE /WX kernel32.lib user32.lib Ws2_32.lib  
 
@@ -28,11 +28,11 @@ IF "%opt1%"=="release" (
 
 
 :DEBUG_FLAGS
-set CompilerFlags=%CompilerFlags% -Zo -Zi -Od -DDEBUG=1
+set CompilerFlags=%CompilerFlags% /Zo /Zi /Od /DDEBUG=1
 goto START_COMPILE
 
 :RELEASE_FLAGS
-set CompilerFlags=%CompilerFlags%  -Zo -Zi -O2
+set CompilerFlags=%CompilerFlags%  /Zo /Zi /O2
 set LinkFlags=%LinkFlags% /OPT:REF,ICF=1
 
 :START_COMPILE
@@ -50,8 +50,7 @@ IF "%VCINSTALLDIR%"=="" (
 )
 
 @echo on
-cl %CompilerFlags% ..\src\win32_msvc.c ..\src\win32_twitch_client.cpp ^
-/Fe%ExeName% /link %LinkFlags%
+cl %CompilerFlags% ..\src\win32_msvc.c ..\src\win32_twitch_client.cpp /Fe%ExeName% /link %LinkFlags%
 
 @echo off
 popd
